7f36568b53411a18aeace5105a82018c929732e5
madelyn714 <lowml-pm23@student.tarc.edu.my>
2026-08-11 22:51:09 +0800

    1

---
+++ DSA_Assignment/src/adt/BSTInterface.java
@@ -0,0 +1,16 @@
+package adt;
+
+public interface BSTInterface<T> {
+
+    public boolean add(int key, T newEntry);
+
+    public T search(int key);
+
+    public boolean contains(int key);
+
+    public boolean isEmpty();
+
+    public void clear();
+
+    public int getNumberOfEntries();
+}
