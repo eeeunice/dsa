e7d8a9c93f98e5e74c7c263cbbb556444fdecdab
lim0827 <limchunchuan341@gmail.com>
2026-08-19 17:41:42 +0800

    1

--- DSA_Assignment/src/dao/GuestData.java
+++ DSA_Assignment/src/dao/GuestData.java
@@ -22,31 +22,31 @@ public class GuestData {
         guestList.add(g3);
 
         Guest g4 = new Guest(10000004, "Eric Loo ", 'M', "011-27652348", "Presidential Suite", 1, 3);
-        g4.setStatus("Waiting");
+        g4.setStatus("Served");
         guestList.add(g4);
 
         Guest g5 = new Guest(10000005, "lim", 'M', "011-67895423", "Double", 2, 5);
-        g5.setStatus("Waiting");
+        g5.setStatus("Served");
         guestList.add(g5);
 
         Guest g6 = new Guest(10000006, "Mohamad bin abdullah", 'M', "012-4627290", "Suite", 1, 10);
-        g6.setStatus("Waiting");
+        g6.setStatus("Served");
         guestList.add(g6);
         
         Guest g7 = new Guest(10000007, "Ong Zi YU", 'F', "011-24627096", "Presidential Suite", 1, 7);
-        g7.setStatus("Waiting");
+        g7.setStatus("Served");
         guestList.add(g7);
         
         Guest g8 = new Guest(10000008, "Syamel Aiman", 'M', "014-5558890", "Suite", 2, 10);
-        g8.setStatus("Waiting");
+        g8.setStatus("Served");
         guestList.add(g8);
         
         Guest g9 = new Guest(10000009, "Beh Bing Hong", 'M', "016-7620990", "Suite", 1, 4);
-        g9.setStatus("Waiting");
+        g9.setStatus("Served");
         guestList.add(g9);
         
         Guest g10 = new Guest(10000010, "Tan Bee Choo", 'F', "011-88889232", "Double", 1, 1);
-        g10.setStatus("Waiting");
+        g10.setStatus("Served");
         guestList.add(g10);
 
         return guestList;
